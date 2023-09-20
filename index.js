f51aff3a252663c22d906b723a443317ed238425
Drinkmilk <141216114+lndvng@users.noreply.github.com>
2023-09-19 23:14:55 -0700

    added shape classes, generate svg function and added comments

--- index.js
+++ index.js
@@ -1,47 +1,71 @@
-// Require packages needed for application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// Array of questions for user input 
-const questions = [
-    {
-      type: 'input',
-      name: 'text',
-      message: 'Enter characters for logo (3 characters max)',
-    },
-    {
-      type: 'input',
-      message: 'Enter text color',
-      name: 'textColor',
-    },
-    {
-      type: 'checkbox',
-      message: 'Select a shape',
-      name: 'shape',
-      choices: ['Circle', 'Triangle', 'Square'],
-    },
-    {
-        type: 'input',
-        message: 'Enter shape color',
-        name: 'shapeColor',
-      },
-  ];
+// Import the Circle class from shapes.js
+const Circle = require('./lib/shapes');
+const Triangle = require('./lib/shapes');
+const Square = require('./lib/shapes');
 
-// // Function to write SVG file
-fs.writeFile(fileName, svgString, (err) => {
-    err ? console.error(err) : console.log('Generated logo.svg');
-});
+// Function to generate SVG string based on user input
+function generateSVG(answers) {
+  // if user chooses circle for shape, svg will render circle logo
+  if (answers.shape == 'Circle') {
+    let newCircle = new Circle(answers.text, answers.textColor, answers.shapeColor)
+    return newCircle.render();
+ // if user chooses triangle for shape, svg will render triangle logo
+  } else if (answers.triangle == 'Triangle') {
+    let newTriangle = new Triangle(answers.text, answers.textColor, answers.shapeColor)
+    return newTriangle.render();
+ // if user chooses square for shape, svg will render square logo
+  } else if (answers.square == 'Square') {
+    let newSquare = new Square(answers.text, answers.textColor, answers.shapeColor)
+    return newSquare.render();
+  }
+}
+
+// Function to write SVG file
+function writeToFile(fileName, svgString) {
+  fs.writeFile(fileName, svgString, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Generated logo.svg');
+    }
+  });
+}
+
+// Array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'text',
+    message: 'Enter characters for logo (3 characters max)',
+  },
+  {
+    type: 'input',
+    message: 'Enter text color',
+    name: 'textColor',
+  },
+  {
+    type: 'list',
+    message: 'Select a shape',
+    name: 'shape',
+    choices: ['Circle', 'Triangle', 'Square'],
+  },
+  {
+    type: 'input',
+    message: 'Enter shape color',
+    name: 'shapeColor',
+  },
+];
 
 // Function to initialize app
 function init() {
-    inquirer.prompt(questions).then(
-        function (answers) {
-        let answers = generate(data);
-        writeToFile('./examples/logo.svg', answers);
-        console.log(answers);
-        }
-    )
+  inquirer.prompt(questions).then((answers) => {
+    const svgString = generateSVG(answers);
+    writeToFile('./examples/logo.svg', svgString);
+  });
 }
 
-//Function call to initialize app
-init()
+// Function call to initialize app
+init();
